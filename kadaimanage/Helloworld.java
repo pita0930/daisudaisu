f6bf043f4acc5b00150feebcf52b4784ea34db77
pita0930 <teohyuki09300725@docomo.ne.jp>
2019-07-08 20:09:32 +0900

    finish print

--- kadaimanage/Helloworld.java
+++ kadaimanage/Helloworld.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 public class Helloworld{
-
-
     public static void main(String[] args){
         Random rnd = new Random();
         System.out.println("Rolling the dice....");
@@ -13,6 +11,10 @@ public class Helloworld{
         System.out.println("Die 1: " + dice1 + "\n" + "Die 2: " + dice2);
         System.out.println("Total value: " + (dice1+dice2));
 
-
+        if(dice1+dice2 > 7){
+            System.out.println("You won!");
+        }else{
+            System.out.println("You lose!");
+        }
     }
 }
